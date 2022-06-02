6f7d7b90fbaaa56db8c90f3657ec14aa9ba34896
gefgu <gefgu@hotmail.com>
2022-06-02 16:06:44 -0300

    Add bold marking to letters

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           const letters = ["a", "b", "c", "d", "e"];
           return (
             <p className="answer" key={answer}>
-              {letters[index]}) {answer}
+              <span className="letter">{letters[index]})</span> {answer}
             </p>
           );
         })}
