0c085fd1583b9f5801ca4782b1031af36fd4f700
gefgu <gefgu@hotmail.com>
2022-06-02 15:12:10 -0300

    Add html layout of app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import generateExpression from "./modules/expressionGenerator";
+import solveExpression from "./modules/expressionSolver";
 
 function App() {
-  return <div>{generateExpression()}</div>;
+  const expression = generateExpression();
+  const correctResult = solveExpression(expression);
+
+  return (
+    <div className="container">
+      <div className="question">
+        <h1>Calcule:</h1>
+        <h2>{expression}</h2>
+      </div>
+      <div>
+        <p>a) {correctResult}</p>
+        <p>b) {correctResult + 2}</p>
+        <p>c) {correctResult - 5}</p>
+        <p>d) {correctResult + 7}</p>
+        <p>e) {correctResult - 10}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
