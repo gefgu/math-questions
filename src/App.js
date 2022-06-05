2f5004fa7d0b3e6cc3cc72c20bd295ea0b21b018
gefgu <gefgu@hotmail.com>
2022-06-05 09:14:58 -0300

    Add bar below question

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
       <div className="question">
         <h1>Resolva a Expressão:</h1>
         <h2 className="expression">{expression}</h2>
+        <hr className="bar"/>
       </div>
       <div>
         {answers.map((answer, index) => {
