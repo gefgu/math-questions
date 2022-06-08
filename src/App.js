f7a308cb230788de92193b2abd70557a468dfa5b
gefgu <gefgu@hotmail.com>
2022-06-08 16:45:34 -0300

    Add steps resolution in variable and remove answer section

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
 import "./styles/fonts.css";
 import "./styles/app.css";
 
-import generateExpression from "./modules/expressionGenerator";
-import {
-  getFourWrongAnswersFromExpression,
-  solveExpression,
-} from "./modules/expressionSolver";
-import shuffle from "./modules/shuffleArray";
 import Question from "./components/Question";
-import { evaluate } from "mathjs";
-import { solveEquation, simplifyExpression } from "mathsteps";
+import { simplifyExpression } from "mathsteps";
 
 function App() {
-  const expression = generateExpression();
-  const correctResult = evaluate(expression);
-  // console.log(correctResult);
-  // console.log(evaluate(expression.replace("x", "*").replace("÷", "/")));
+  // const expression = generateExpression();
+  const expression = "6 + 3 * 4 - 8 / 4";
 
-  const steps = simplifyExpression();
-  console.log(steps);
-
-  // console.log(steps[0].newEquation.ascii());
+  let steps = simplifyExpression(expression);
 
   console.log(expression);
-  steps.forEach((step) => {
-    console.log(step.newNode.toString()); // after change: 6 x
-  });
-
-  const wrongAnswers = getFourWrongAnswersFromExpression(
-    expression,
-    correctResult
-  );
-  const answers = shuffle(wrongAnswers.concat(correctResult));
+  steps = steps.map((step) => step.newNode.toString());
+  console.log(steps);
 
   return (
     <div className="container">
-      <Question expression={expression.replaceAll("*", "x").replaceAll("/", "÷")} />
-      <div>
-        {answers.map((answer, index) => {
-          const letters = ["a", "b", "c", "d", "e"];
-          return (
-            <p className="answer" key={answer}>
-              <span className="letter">{letters[index]})</span> {answer}
-            </p>
-          );
-        })}
-      </div>
+      <Question
+        expression={expression.replaceAll("*", "x").replaceAll("/", "÷")}
+      />
+
     </div>
   );
 }
