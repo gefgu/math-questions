ef2f5e6f747ae974ed2a8d24f0728ce54a640b29
gefgu <gefgu@hotmail.com>
2022-06-10 20:41:51 -0300

    Add background animation

--- src/App.js
+++ src/App.js
@@ -5,19 +5,59 @@ import Question from "./components/Question";
 import { simplifyExpression } from "mathsteps";
 import ResolutionStep from "./components/ResolutionStep";
 import generateExpression from "./modules/expressionGenerator";
+import anime from "animejs/lib/anime.es";
+import { useEffect, useRef } from "react";
 
 function App() {
   const expression = generateExpression();
 
   let steps = simplifyExpression(expression);
 
-  console.log(expression);
   steps = steps.map((step) => step.newNode.toString());
   steps.unshift(expression);
-  console.log(steps);
+
+  const containerRef = useRef(null);
+
+  const animateBlocks = () => {
+    anime({
+      targets: ".block",
+      translateX: () => anime.random(0, 500),
+      translateY: () => anime.random(0, containerRef.current.clientHeight),
+      scale: () => anime.random(1, 4),
+      easing: "linear",
+      duration: 2000,
+      delay: anime.stagger(10),
+      complete: animateBlocks,
+    });
+  };
+
+  useEffect(() => {
+    animateBlocks();
+  }, []);
+
+  const startingBlockStyle = () => {
+    return {
+      transform: `translateX(${Math.floor(
+        Math.random() * 499
+      )}px) translateY(${Math.floor(Math.random() * 200)}px) scale(${Math.floor(
+        Math.random() * 4
+      )})`,
+    };
+  };
+
+  console.log(startingBlockStyle());
 
   return (
-    <div className="container">
+    <div className="container" ref={containerRef}>
+      {[...Array(100).keys()].map((value) => {
+        return (
+          <div
+            className="block"
+            key={value * 10 + 1}
+            style={startingBlockStyle()}
+          ></div>
+        );
+      })}
       <Question
         expression={expression.replaceAll("*", "x").replaceAll("/", "÷")}
       />
