cb9b1f8f58da3d9c39e83766568ab73703966fe7
gefgu <gefgu@hotmail.com>
2022-06-12 09:23:54 -0300

    Update tests to use hard coded values

--- src/modules/answerGenerator.test.js
+++ src/modules/answerGenerator.test.js
@@ -1,19 +1,16 @@
 import getAnswersFromExpression from "./answerGenerator";
-import { evaluate } from "mathjs";
 
 describe("Give appropriate answers", () => {
-  const patternSumAndMinus = /\d[-+]\d/g;
-
   test("Give Correct Answers", () => {
     const expression = "8 - 9 * (4 + 2) / 9";
     const answers = getAnswersFromExpression(expression);
-    expect(answers.includes(evaluate(expression))).toBe(true);
+    expect(answers.includes(2)).toBe(true);
   });
 
   test("Ignore parentheses", () => {
-    const expression = "8 - 9 * (4 + 2) / 9";
+    const expression = "8 - 9 * (4 + 2) / 2";
     const answers = getAnswersFromExpression(expression);
-    expect(answers.includes(evaluate(expression.replace(/[)(]/g, "")))).toBe(
+    expect(answers.includes(-27)).toBe(
       true
     );
   });
@@ -21,11 +18,6 @@ describe("Give appropriate answers", () => {
   test("Sum and subtractions first", () => {
     const expression = "8 - 4 * 4 + 2 / 3";
     const answers = getAnswersFromExpression(expression);
-    let wrongExpression = expression;
-    while (wrongExpression.match(patternSumAndMinus).length > 1) {
-      const match = wrongExpression.match(patternSumAndMinus)[0];
-      wrongExpression = wrongExpression.replace(match, evaluate(match));
-    }
 
     expect(answers.includes(8)).toBe(true);
   });
