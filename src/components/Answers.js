1df594419d190b7f596455b6122b3ffbea3c84c4
gefgu <gefgu@hotmail.com>
2022-06-12 10:23:44 -0300

    Add answer cleaning for display

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -3,6 +3,12 @@ const Answers = ({ answers }) => {
     <div>
       {answers.map((answer, index) => {
         const letters = ["a", "b", "c", "d", "e"];
+        answer = `${answer}`;
+        answer = answer.replace(".", ",");
+        try {
+          const trailingZeros = answer.match(/,[1-9]*(0+$)/)[1];
+          answer = answer.replace(trailingZeros, "");
+        } catch (e) {}
         return (
           <p className="answer" key={answer}>
             <span className="letter">{letters[index]})</span> {answer}
