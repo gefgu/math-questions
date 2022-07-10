f327c19420444e713d1c2623167e957830fd71ee
gefgu <gefgu@hotmail.com>
2022-07-10 10:36:12 -0300

    Add single responsability to generate expression

--- src/modules/expressionGenerator.js
+++ src/modules/expressionGenerator.js
@@ -1,33 +1,18 @@
-import { evaluate } from "mathjs";
-
 const generateExpression = () => {
   const expressionTemplates = [
     "a + b * c - d / e",
-    "(a + b) * c - d / e",
-    "a - b * (c + d) / e",
     "a - b / c + d * e",
     "a / b + c * d / e",
     "a - b * c + d * e",
     "a * b / c + d - e",
   ];
 
-  let resultIsInteger = false;
-  let finalExpression;
-  do {
-    let expression =
-      expressionTemplates[
-        Math.floor(Math.random() * expressionTemplates.length)
-      ];
-    ["a", "b", "c", "d", "e"].forEach((letter) => {
-      expression = expression.replace(
-        letter,
-        Math.floor(Math.random() * 9 + 1)
-      );
-    });
-    resultIsInteger = Number.isInteger(evaluate(expression));
-    if (resultIsInteger) finalExpression = expression;
-  } while (!resultIsInteger);
-  return finalExpression;
+  let expression =
+    expressionTemplates[Math.floor(Math.random() * expressionTemplates.length)];
+  ["a", "b", "c", "d", "e"].forEach((letter) => {
+    expression = expression.replace(letter, Math.floor(Math.random() * 9 + 1));
+  });
+  return expression;
 };
 
 export default generateExpression;
