257a0cf32023fe095c92cf624685b364d21af926
gefgu <gefgu@hotmail.com>
2022-07-10 15:49:52 -0300

    Refactor answer component to work better with 2 answers

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,22 +1,19 @@
 const Answers = ({ answers }) => {
+  answers = answers.map((answer, index) => {
+    answer = `${answer}`;
+    answer = answer.replace(".", ",");
+    try {
+      const trailingZeros = answer.match(/,[1-9]*(0+$)/)[1];
+      answer = answer.replace(trailingZeros, "");
+    } catch (e) {}
+    return answer;
+  });
+
   return (
     <div className="answers">
-      {answers.map((answer, index) => {
-        answer = `${answer}`;
-        answer = answer.replace(".", ",");
-        try {
-          const trailingZeros = answer.match(/,[1-9]*(0+$)/)[1];
-          answer = answer.replace(trailingZeros, "");
-        } catch (e) {}
-        return (
-          <>
-            <p className="answer" key={answer}>
-              {answer}
-            </p>
-            {index === 0 && <p className="answer letter">OU</p>}
-          </>
-        );
-      })}
+      <p className="answer">{answers[0]}</p>
+      <p className="answer letter">OU</p>
+      <p className="answer">{answers[1]}</p>
     </div>
   );
 };
