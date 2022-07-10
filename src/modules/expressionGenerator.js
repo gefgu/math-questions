7ee32729e2efe2440fdb3a4ae1876db479323332
gefgu <gefgu@hotmail.com>
2022-07-10 10:47:58 -0300

    Add more numbers to expression generator

--- src/modules/expressionGenerator.js
+++ src/modules/expressionGenerator.js
@@ -14,7 +14,7 @@ const generateExpression = () => {
   let expression =
     expressionTemplates[Math.floor(Math.random() * expressionTemplates.length)];
   ["a", "b", "c", "d", "e"].forEach((letter) => {
-    expression = expression.replace(letter, Math.floor(Math.random() * 9 + 1));
+    expression = expression.replace(letter, Math.floor(Math.random() * 10 + 1));
   });
   return expression;
 };
